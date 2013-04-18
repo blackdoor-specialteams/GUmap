9303a4d0f0106276a0c12d3a23b8ca69269e1a47
Nathan Fischer <nfischer3@zagmail.gonzaga.edu>
2013-04-17 18:22:03 -0700

    trying to get a map

--- gen/com/blackdoor/gumap/R.java
+++ gen/com/blackdoor/gumap/R.java
@@ -25,7 +25,7 @@ public final class R {
     }
     public static final class id {
         public static final int action_settings=0x7f080002;
-        public static final int pager=0x7f080000;
+        public static final int map=0x7f080000;
         public static final int section_label=0x7f080001;
     }
     public static final class layout {
