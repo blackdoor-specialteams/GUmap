26536e08325d948ccbbdff5da977d666d488e70d
Cj Buresch <cjburesch@gmail.com>
2013-04-21 21:03:42 -0700

    BuildingInfo Class in progress, incremental update
    +BuidlingInfo class
    +Working on Scrollable info class, displays default values for each
    section, to be overwritten before display by the populate function.
    +Will handle edge cases (null values and whatnot)
    +Having fun all alone in this room.....i guess.

---
+++ src/com/blackdoor/gumap/BuildingInfo.java
@@ -0,0 +1,60 @@
+package com.blackdoor.gumap;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import java.util.Locale;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.NavUtils;
+import android.support.v4.view.ViewPager;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class BuildingInfo extends Activity {
+
+	private GUBuildingMarker building;
+	private TextView BuidlingName;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		
+		
+		
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_building_info);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.building_info, menu);
+		return true;
+	}
+	
+	public void PopulateText()
+	{
+		setBuildingName();
+		
+	}
+	
+	private void setBuildingName()
+	{
+		BuidlingName = (TextView) findViewById(R.id.Bname);
+		BuidlingName.setText(building.getName());
+	}
+	
+	
+}
