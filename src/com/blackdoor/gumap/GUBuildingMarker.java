e0a7324a6d414aa1c10112e5d7d94d01378717a8
Nathan Fischer <nfischer3@zagmail.gonzaga.edu>
2013-04-29 13:21:11 -0700

    removed unneeded imports

--- src/com/blackdoor/gumap/GUBuildingMarker.java
+++ src/com/blackdoor/gumap/GUBuildingMarker.java
@@ -1,34 +1,10 @@
 package com.blackdoor.gumap;
 
-import java.util.Locale;
-
-import blackdoor.util.*;
-
 import com.blackdoor.gumap.MainActivity.Zoom;
-import com.google.android.gms.maps.*;
 import com.google.android.gms.maps.model.*;
 
-import android.app.ActionBar;
-import android.app.Activity;
-import android.app.FragmentTransaction;
-import android.os.Bundle;
 import android.os.Parcel;
 import android.os.Parcelable;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.app.NavUtils;
-import android.support.v4.view.ViewPager;
-import android.util.Log;
-import android.view.Gravity;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 public class GUBuildingMarker implements Parcelable{
 	Marker	closeMarker;
